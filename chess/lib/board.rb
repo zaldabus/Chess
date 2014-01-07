474d0de9679500ed226b30379c7dcae406a06863
Alex Guo and Ryan Beckman <alex+ryan@appacademy.io>
2014-01-07 16:05:21 -0500

    Board creates

--- chess/lib/board.rb
+++ chess/lib/board.rb
@@ -2,8 +2,42 @@ require 'pieces'
 
 class Board
 
-  # @board is the 2d array
-  # attr_reader :board
+  def self.generate_board
+    Array.new(8) { Array.new(8) }
+  end
+
+  def initialize(board = self.class.generate_board)
+    @board = board
+    populate_board
+  end
+
+  def populate_board
+    make_complex_row(0, :white)
+    make_pawn_row(1, :white)
+    make_complex_row(7, :black)
+    make_pawn_row(6, :black)
+  end
+
+  def make_complex_row(row, color)
+    (0..7).each do |i|
+      case i
+      when 0, 7
+        @board[[i, row]] = Rook.new(self, [i, row], color)
+      when 1, 6
+        @board[[i, row]] = Knight.new(self, [i, row], color)
+      when 2, 5
+        @board[[i, row]] = Bishop.new(self, [i, row], color)
+      when 3
+        @board[[i, row]] = Queen.new(self, [i, row], color)
+      when 4
+        @board[[i, row]] = King.new(self, [i, row], color)
+      end
+    end
+  end
+
+  def make_pawn_row(row, color)
+     (0..7).each do |i| {|i| @board[[i, row]] = Pawn.new(self, [i, row], color)}
+  end
 
   def [](pos)
     x, y = pos[0], pos[1]
